19e8dd5cefe642c7887a467400b55ece6a3e17a7
Vladimir Denisov <vdenisov.dev@gmail.com>
2021-02-16 19:17:01 +0300

    03. try simple app debugging via "console" tool

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: "ns-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
     message: string = 'All is okay!!!';
 
-    ngOnInit() {
-        console.log('init');
+    constructor() {
+        console.log('Hello World');
+        console.dir({ type: 'Apple', color: 'Red' });
     }
 }
