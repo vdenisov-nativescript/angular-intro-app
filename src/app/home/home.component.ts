8fd5529b2fdbe3323e40c0f9b39e22d1f3365e60
Vladimir Denisov <vdenisov.dev@gmail.com>
2021-02-16 19:00:37 +0300

    02. add animation to home page and change app theme

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: "ns-home",
   templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
     message: string = 'All is okay!!!';
