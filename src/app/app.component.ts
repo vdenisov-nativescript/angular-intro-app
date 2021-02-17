02c4800e91002b09f601427011f77a6d9be724db
Vladimir Denisov <vdenisov.dev@gmail.com>
2021-02-17 13:56:29 +0300

    08. add user model and move login template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from "@angular/core";
+import { User } from './shared/user/user';
 
 @Component({
     selector: "ns-app",
-    templateUrl: "./app.component.html",
-    styleUrls: [
-        "./pages/login/login-common.css",
-        "./pages/login/login.css",
-    ]
+    templateUrl: "./pages/login/login.component.html",
+    styleUrls: ["./pages/login/login-common.css", "./pages/login/login.css"]
 })
 export class AppComponent {
-    public email = 'test@gmail.com';
+    public user: User;
     public isLoggingIn = true;
 
+    constructor() {
+        this.user = new User()
+    }
+
     public submit() {
-        alert('You\'re using: ' + this.email);
+        alert('You\'re using: ' + this.user.email);
     }
 
     public toggleDisplay() {
